36c4b03f1cf3e9877bdabff4e622e8c4cbec2b96
reikkundesu <manalojosephkitrei@gmail.com>
2021-11-11 17:37:33 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAQTcPu9d0m475kt--RmuRonurX_-zsL4c",
+    authDomain: "midtermasi.firebaseapp.com",
+    databaseURL: "https://midtermasi-default-rtdb.asia-southeast1.firebasedatabase.app/",
+    projectId: "midtermasi",
+    storageBucket: "midtermasi.appspot.com",
+    messagingSenderId: "317726761917",
+    appId: "1:317726761917:web:79bda967395750419fe8d4",
+    measurementId: "G-0WEMMB444X"
+    };
+    
+    firebase.initializeApp(firebaseConfig);
+
+    firebase.auth.Auth.Persistence.LOCAL;
+
+    $("#btn-login").click(function()
+    {
+        var email = $("#email").val();
+        var password = $("#password").val();
+
+        if(email !="" && password !="")
+        {
+            var result = firebase.auth().signInWithEmailAndPassword(email, password);
+
+            result.catch(function(error)
+            {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+
+                console.log(errorCode);
+                console.log(errorMessage);
+
+                window.alert("Messaage: " + errorMessage);
+            });
+        }
+        else
+        {
+            window.alert("Form is incomplete. Please fill out all fields.");
+        }
+    });
